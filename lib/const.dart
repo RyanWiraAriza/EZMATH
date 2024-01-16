8fcf6a7f092182dd3ee6a94e8ea5165169fda558
RyanWiraAriza <127129798+RyanWiraAriza@users.noreply.github.com>
2024-01-16 21:21:25 +0700

    Delete lib directory

--- lib/const.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-const OPENAI_API_KEY = "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";
-var whiteTextStyle = const TextStyle(
-  fontWeight: FontWeight.bold,
-  fontSize: 32,
-  color: Colors.white,
-);
--- lib/pages/responsive/dimensions.dart
+++
@@ -1,2 +0,0 @@
-// set the common device dimensions here for mobile / tablet / desktop
-const mobileWidth = 600;
